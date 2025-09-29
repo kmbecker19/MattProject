8424f5562ef2caa44e38dbde26bc8fd4ca1048e0
Kyle Becker <kmb08625@uga.edu>
2025-09-29 14:48:54 -0400

    Update device fixtures

--- test/test_mangle_device_name.py
+++ test/test_mangle_device_name.py
@@ -5,21 +5,43 @@ from filter_json import mangle_device_names, get_device_mappings
 @pytest.fixture
 def example_jsonl_data():
     return [
-        {"event": {"device": "MD=CISCO_EPNM!ND=DeviceA", "info": "Some info"}},
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DeviceB",
-                "info": "Other info",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DeviceA",
+                    "info": "Some info",
+                }
             }
         },
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DeviceA",
-                "info": "Repeated device",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DeviceB",
+                    "info": "Other info",
+                }
             }
         },
-        {"event": {"device": "MD=CISCO_EPNM!ND=DeviceC", "info": "More info"}},
-        {"event": {"device": "DeviceD", "info": "Different format"}},
+        {
+            "event": {
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DeviceA",
+                    "info": "Repeated device",
+                }
+            }
+        },
+        {
+            "event": {
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DeviceC",
+                    "info": "More info",
+                }
+            }
+        },
+        {
+            "event": {
+                "properties": {"device": "DeviceD", "info": "Different format"}
+            }
+        },
     ]
 
 
@@ -38,29 +60,44 @@ def example_mangled_data():
     return [
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DEVICE-001",
-                "info": "Some info",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DEVICE-001",
+                    "info": "Some info",
+                }
             }
         },
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DEVICE-002",
-                "info": "Other info",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DEVICE-002",
+                    "info": "Other info",
+                }
             }
         },
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DEVICE-001",
-                "info": "Repeated device",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DEVICE-001",
+                    "info": "Repeated device",
+                }
             }
         },
         {
             "event": {
-                "device": "MD=CISCO_EPNM!ND=DEVICE-003",
-                "info": "More info",
+                "properties": {
+                    "device": "MD=CISCO_EPNM!ND=DEVICE-003",
+                    "info": "More info",
+                }
             }
         },
-        {"event": {"device": "DEVICE-004", "info": "Different format"}},
+        {
+            "event": {
+                "properties": {
+                    "device": "DEVICE-004",
+                    "info": "Different format",
+                }
+            }
+        },
     ]
 
 

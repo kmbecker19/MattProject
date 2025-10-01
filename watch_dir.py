de7232edc2f5e4852a8c9f7ca902ef8f90fda044
Kyle Becker <kmb08625@uga.edu>
2025-10-01 13:14:45 -0400

    Add json mangling to directory watch script

--- watch_dir.py
+++ watch_dir.py
@@ -1,25 +1,33 @@
 import logging
+from pathlib import Path
 from watchdog.observers import Observer
 from watchdog.events import (
-    LoggingEventHandler,
     FileSystemEventHandler,
     FileCreatedEvent,
     FileModifiedEvent,
 )
+from filter_json import mangle_json_file
 
 
 class CustomEventHandler(FileSystemEventHandler):
     def on_created(self, event):
         if isinstance(event, FileCreatedEvent):
-            file_path = event.src_path
-            logging.info(f"File created: {file_path}")
-            # TODO: Implement running python script
+            self._handle_mangling(event)
 
     def on_modified(self, event):
         if isinstance(event, FileModifiedEvent):
-            file_path = event.src_path
-            logging.info(f"File modified: {file_path}")
-            # TODO: Implement running python script
+            self._handle_mangling(event)
+
+    def _handle_mangling(self, event):
+        file_path = Path(event.src_path)
+        if file_path.suffix in ('.json', '.jsonl'):
+            filename = file_path.name
+            data_dir = file_path.parent.parent
+            output_dir = data_dir / "output"
+            mangle_json_file(file_path, output_dir / filename, [])
+            logging.info(
+                f"Processed file '{file_path}' and saved to '{output_dir}'"
+            )
 
 
 def log_filesystem_change(path="."):
@@ -33,13 +41,17 @@ def log_filesystem_change(path="."):
         format="%(asctime)s - %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
-    event_handler = LoggingEventHandler()
+    event_handler = CustomEventHandler()
     observer = Observer()
     observer.schedule(event_handler, path, recursive=True)
     observer.start()
     try:
-        while observer.isAlive():
+        while observer.is_alive():
             observer.join(1)
     finally:
         observer.stop()
         observer.join()
+
+
+if __name__ == "__main__":
+    log_filesystem_change(path="./data/input")
